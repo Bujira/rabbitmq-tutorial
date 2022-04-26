910f1ef7657520c30fdf6158eb388884194387b3
Bujira <yurifcorrea@gmail.com>
2022-04-26 13:51:51 -0300

    commit 2: producer/sender refactor

--- send.js
+++ send.js
@@ -6,22 +6,21 @@ amqp.connect('amqp://localhost', (error0, connection) => {
   if (error0) {
     throw error0;
   }
-
   connection.createChannel((error1, channel) => {
     if (error1) {
       throw error1;
     }
 
     var queue = 'hello';
-    var msg = 'Hello world';
+    var msg = 'Hello World!';
 
     channel.assertQueue(queue, {
       durable: false
     });
-
     channel.sendToQueue(queue, Buffer.from(msg));
-  });
 
+    console.log(" [x] Sent %s", msg);
+  });
   setTimeout(() => {
     connection.close();
     process.exit(0);
